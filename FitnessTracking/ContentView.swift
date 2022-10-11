447696c4cb0397a028cfd74d9232b7dfe6544e7e
Matthew Kolnicki <parkourmatt@hotmail.com>
2022-10-10 23:53:36 -0400

    Initial Commit

---
+++ FitnessTracking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessTracking
+//
+//  Created by Matthew Kolnicki on 10/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitnessTracking/FitnessTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackingApp.swift
+//  FitnessTracking
+//
+//  Created by Matthew Kolnicki on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
