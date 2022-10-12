3aa2125440d3e15556163d12b51320f2df42d419
Matthew Kolnicki <parkourmatt@hotmail.com>
2022-10-12 15:20:19 -0400

    Added FaceID

--- FitnessTracking/ContentView.swift
+++ FitnessTracking/ContentView.swift
@@ -1,21 +1,45 @@
-//
 //  ContentView.swift
 //  FitnessTracking
 //
-//  Created by Matthew Kolnicki on 10/10/22.
+//  Matthew Kolnicki, Jalal Jean-Charles, Noah Shaffer, Randy Toyberman
 //
-
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    @State private var unlocked = false
+    @State private var text = "LOCKED"
     var body: some View {
-        Text("Hello, world!")
+        Text(text)
             .padding()
+        Button("Authenticate"){
+            authenticate()
+        }
+    }
+    func authenticate(){
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
+                                   localizedReason: "This is for security"){ success, authenticationError in
+                if success{
+                    text = "UNLOCKED"
+                } else {
+                    text = "There was a problem"
+                    
+                }
+            }
+        } else {
+            text = "This phone does not have biometrics"
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
