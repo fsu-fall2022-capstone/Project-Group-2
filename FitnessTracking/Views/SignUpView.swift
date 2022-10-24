abb45b947d121b33fe72325998aab8dc71f4ac97
Jalal Jean-Charles <joj19a@fsu.edu>
2022-10-23 20:46:36 -0400

    Added navigation to both signup and login buttons on signup view

--- FitnessTracking/Views/SignUpView.swift
+++ FitnessTracking/Views/SignUpView.swift
@@ -11,9 +11,11 @@ struct SignUpView: View {
     @State private var username = ""
     @State private var password = ""
     @State private var confirm = ""
-    @State private var firstname = ""
-    @State private var lastname = ""
+    @State private var firstName = ""
+    @State private var lastName = ""
     @State private var email = ""
+    @State private var newUser = false
+    @State private var currentUser = false
     
     
     var body: some View {
@@ -23,11 +25,11 @@ struct SignUpView: View {
                         .font(.largeTitle)
                         .bold()
                         .padding()
-                    TextField("Enter First Name", text: $firstname)
+                    TextField("Enter First Name", text: $firstName)
                         .padding()
                         .frame(width: 300, height: 50)
                         .cornerRadius(10)
-                    TextField("Enter Last Name", text: $lastname)
+                    TextField("Enter Last Name", text: $lastName)
                         .padding()
                         .frame(width: 300, height: 50)
                         .cornerRadius(10)
@@ -44,12 +46,27 @@ struct SignUpView: View {
                         .frame(width: 300, height: 50)
                         .cornerRadius(10)
                     Button("Sign Up") {
+                        newUser = true;
                     }.padding()
                         .frame(width: 300, height: 50)
                         .foregroundColor(.white)
                         .background(Color.black)
                         .cornerRadius(10)
-                    Text("Already a user? LOGIN")
+                    Text("Already a user?")
+                    Button("LOGIN") {
+                        currentUser = true
+                    }.padding()
+                        .foregroundColor(.black)
+                        .background(.white)
+                        .bold(true)
+                    NavigationLink(destination: LandingView(), isActive: $currentUser) {
+                        EmptyView()
+                    }
+                }
+                HStack{
+                    NavigationLink(destination: HomeView(), isActive: $newUser) {
+                        EmptyView()
+                    }
                 }
             }
     }
