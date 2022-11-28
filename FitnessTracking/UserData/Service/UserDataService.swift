ba455dbf494559c9300dbfe0c32250b591076fd0
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2022-11-28 14:03:57 -0500

    Update UserDataService.swift

--- FitnessTracking/UserData/Service/UserDataService.swift
+++ FitnessTracking/UserData/Service/UserDataService.swift
@@ -8,6 +8,71 @@
 
 import Foundation
 import Combine
+import FirebaseDatabase
+import Firebase
+
+enum UserKeys: String{
+    case firstName
+    case lastName
+}
+
+protocol UserDataService {
+    func UserData(with details: UserDataDetails) -> AnyPublisher<Void, Error>
+}
+
+final class UserDataImpl: UserDataService{
+    
+    func UserData(with details: UserDataDetails) -> AnyPublisher<Void, Error>{
+        Deferred{
+            
+            Future { promise in
+                Auth.auth().createUser(withEmail: details.gender,
+                                       password: details.height) { result, error in
+                    if let err = error {
+                        print(error!.localizedDescription)
+                        promise(.failure(err))
+                    }
+                    else{
+                        
+                        if let uid = result?.user.uid{
+                            
+                            let values = [dbKeys.firstName.rawValue: details.weight,
+                                          dbKeys.lastName.rawValue: details.age] as [String : Any]
+                            
+                            Database.database()
+                                .reference()
+                                .child("users")
+                                .child(uid)
+                                .updateChildValues(values)  { error, ref in
+                                    
+                                    if let err = error {
+                                        print(error!.localizedDescription)
+                                        promise(.failure(err))
+                                    }
+                                    else {
+                                        promise(.success(()))
+                                    }
+                                }
+                            
+                        }
+                        else{
+                            promise(.failure(NSError(domain: "Invalid", code: 0)))
+                        }
+                        
+                    }
+                }
+            }
+        }
+        .receive(on: RunLoop.main)
+        .eraseToAnyPublisher()
+    }
+}
+
+
+/*
+
+import Foundation
+import Combine
 import FirebaseAuth
 
 protocol UserDataService{
@@ -37,4 +102,6 @@ final class UserDataServiceImpl: UserDataService {
     }
     
 }
+
+*/
 
