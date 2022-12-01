17101164f09de34bd74bf3a42044fdc289a2348a
rantoybs <randy.toyberman@gmail.com>
2022-12-01 15:51:07 -0500

    Fixed diet view for varying days

--- FitnessTracking/Home/Views/DietView.swift
+++ FitnessTracking/Home/Views/DietView.swift
@@ -10,11 +10,12 @@
 import SwiftUI
 
 struct Diet: View {
+    let diets = DietData
     var body: some View {
         NavigationView {
             VStack {
                 ZStack{
-                    Image("istockphoto-877317520-612x612.jpeg")
+                    Image("istockphoto-877317520-612x612")
                         .resizable()
                         .aspectRatio(contentMode: .fill)
                         .frame(height:300)
@@ -49,21 +50,21 @@ struct Diet: View {
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack {
                         
-                        ForEach(0 ..< 5){ item in
+                        ForEach(diets){ Diet_views in
                             // Day card
                             ZStack {
-                                Image("istockphoto-505020568-612x612.jpeg")
+                                Image(Diet_views.image)
                                     .resizable()
                                     .aspectRatio(contentMode: .fill)
                                     .frame(width: 380, height:220)
                                 
                                 VStack{
                                     Spacer()
-                                    Text("Monday")
+                                    Text(Diet_views.day)
                                         .font(.largeTitle)
                                         .fontWeight(.semibold)
                                         .foregroundColor(.white)
-                                    Text("Calorie Count:")
+                                    Text("Calorie Count: " + Diet_views.calorieCount)
                                         .foregroundColor(.white)
                                 }
                                 .padding()
@@ -87,4 +88,27 @@ struct Diet_Previews: PreviewProvider {
     static var previews: some View {
         DietView()
     }
+
 }
+struct Diet_views: Identifiable{
+    
+    var id = UUID()
+    
+    var day: String
+    var calorieCount: String
+    var image: String
+    
+}
+let DietData = [
+    
+        Diet_views(day: "Monday", calorieCount: "0", image: "istockphoto-505020568-612x612"),
+        Diet_views(day: "Tuesday", calorieCount: "0", image: "istockphoto-505020568-612x612"),
+        Diet_views(day: "Wednesday", calorieCount: "0", image: "istockphoto-505020568-612x612"),
+        Diet_views(day: "Thursday", calorieCount: "0", image: "istockphoto-505020568-612x612"),
+        Diet_views(day: "Friday", calorieCount: "0", image: "istockphoto-505020568-612x612"),
+        Diet_views(day: "Saturday", calorieCount: "0", image: "istockphoto-505020568-612x612"),
+        Diet_views(day: "Sunday", calorieCount: "0", image: "istockphoto-505020568-612x612"),
+    
+    
+    ]
+
