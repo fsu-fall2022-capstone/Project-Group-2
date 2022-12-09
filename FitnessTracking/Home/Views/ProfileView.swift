372c04e585c07ff11bcc86df19b14484cbe16aa4
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2022-12-08 23:07:34 -0500

    Update ProfileView.swift
    Added user details to profile page

--- FitnessTracking/Home/Views/ProfileView.swift
+++ FitnessTracking/Home/Views/ProfileView.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var sessionService: SessionServiceImpl
     var body: some View {
         List {
             Section("Fitness Information") {
-                Text("Height")
+                Text("Last Name: \(sessionService.userDetails?.lastName ?? "N/A")")
+                Text("Gender: \(sessionService.userDetails?.gender ?? "N/A")")
+                Text("Age: \(sessionService.userDetails?.age ?? "N/A")")
+                Text("Height: \(sessionService.userDetails?.height ?? "N/A")in")
+                Text("Weight: \(sessionService.userDetails?.weight ?? "N/A")lbs")
                 Text("Weight")
                 Text("Age")
             }
