d448a6a12df1e48e5048d5cecb8a6513a635ffd2
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2022-12-09 21:02:20 -0500

    Update ProfileView.swift
    BMI ADDED DIRTY ADDITIONS REMOVED

--- FitnessTracking/Home/Views/ProfileView.swift
+++ FitnessTracking/Home/Views/ProfileView.swift
@@ -11,17 +11,14 @@ struct ProfileView: View {
     @EnvironmentObject var sessionService: SessionServiceImpl
     var body: some View {
         List {
-            Section("Fitness Information") {
+             Section("Fitness Information") {
+                Text("First Name: \(sessionService.userDetails?.firstName ?? "N/A")")
                 Text("Last Name: \(sessionService.userDetails?.lastName ?? "N/A")")
                 Text("Gender: \(sessionService.userDetails?.gender ?? "N/A")")
                 Text("Age: \(sessionService.userDetails?.age ?? "N/A")")
                 Text("Height: \(sessionService.userDetails?.height ?? "N/A")in")
                 Text("Weight: \(sessionService.userDetails?.weight ?? "N/A")lbs")
-                Text("Weight")
-                Text("Age")
-            }
-            Section("Goals"){
-                
+                Text("bmi: \(sessionService.userDetails?.bmi ?? "N/A")")
             }
         }
     }
